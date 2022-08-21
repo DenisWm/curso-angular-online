54bd7283ecaa39c3148aa8f3a50f81b3557a806c
DenisWm <denismamoni@hotmail.com>
2022-08-21 17:12:23 -0400

    adicioanndo pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoldPipe } from './bold.pipe';
 import { ContadorComponent } from './contador/contador.component';
 import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
 import { ExercicioDiretivasComponent } from './exercicio-diretivas/exercicio-diretivas.component';
 import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+
+registerLocaleData(localePt, "pt")
 
 @NgModule({
   declarations: [
@@ -26,13 +32,18 @@ import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.componen
     ExercicioNgclassComponent,
     LoginComponent,
     ExtratoComponent,
-    NaoEncontradoComponent
+    NaoEncontradoComponent,
+    ExercicioPipesComponent,
+    BoldPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
